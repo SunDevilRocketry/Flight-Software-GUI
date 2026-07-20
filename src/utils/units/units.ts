5133acbf7e8bb042f45c9d724be9cca4a859e3b0
ETSsound <147210601+ETSsound@users.noreply.github.com>
2026-07-19 21:25:55 -0700

    Add unit conversion utility based on a configurable system unit. Default system units to feet/g-force for flight.

---
+++ src/utils/units/units.ts
@@ -0,0 +1,61 @@
+/* Export all handlers */
+// Altitude
+import { AltitudeUnitsHandler } from "@/utils/units/altitudeUnits"
+export * from "@/utils/units/altitudeUnits"
+// Acceleration
+import { AccelerationUnitsHandler } from "@/utils/units/accelerationUnits"
+export * from "@/utils/units/accelerationUnits"
+
+/* All exported units handlers should be defined here */
+export const altitudeHandler = new AltitudeUnitsHandler();
+export const accelerationHandler = new AccelerationUnitsHandler();
+
+export { type UnitsHandler };
+
+/**
+ * Enumerated type with constant conversion factors used by this module.
+ */
+enum ConversionFactors {
+    METERS_TO_FEET = 3.280839895,
+    GRAVITY_MPS = 9.80665
+}
+export default ConversionFactors;
+
+/**
+ * Interface for all unit converters to follow for portability.
+ */
+interface UnitsHandler {
+    /* Shared members for unitsHandler. Not typed for inheritance reasons. */
+    systemUnits: any
+
+    /**
+     * Convert the internal measurement to the system's configured
+     * display units.
+     * 
+     * @param input The input readout (as SI unit)
+     * @returns The output readout (as system units)
+     */
+    convertToDisplay(input: number): number
+
+    /**
+     * Retrieve a short unit string to append to the readout.
+     * 
+     * @returns A shortened form of the unit string
+     */
+    getDisplayUnitShort(): string
+
+    /**
+     * Retrieve the full unit string to append to the readout.
+     * 
+     * @returns The unit string
+     */
+    getDisplayUnitLong(): string
+
+    /**
+     * Retrieve a string with the correct units for display.
+     * 
+     * @param input The input value (as SI)
+     * @returns A string with the output value and its associated units
+     */
+    getDisplayString(input: number): string
+}
